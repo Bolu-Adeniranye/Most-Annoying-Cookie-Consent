613d1eee9f554a03857db0608f3a3ca82751d249
bolu-sama <68526923+bolu-sama@users.noreply.github.com>
2024-06-18 20:19:54 +0100

    Added the troll consent part

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
+const modalText = document.getElementById("modal-text")
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -7,4 +8,33 @@ setTimeout(function(){
 
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
+})
+
+const cookieForm = document.getElementById("cookie-form")
+
+cookieForm.addEventListener('submit',function(e){
+    e.preventDefault()
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="./loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>`
+
+    setTimeout(function() {
+        document.getElementById("uploadText").textContent = `Making the sale...`
+    }, 1500)
+
+    setTimeout(function(){
+        document.getElementById("modal-inner").innerHTML = ""
+        document.getElementById("modal-inner").innerHTML =   
+            `<h2>Thanks you sucker! </h2>
+            <p>We have obtained the rights to your eternal soul.</p>
+            <div class="idiot-gif">
+                <img src="./ulose.gif">
+            </div>
+                    ` 
+        document.getElementById("modal-inner").style.textAlign = "center"
+    },3000)
 })
